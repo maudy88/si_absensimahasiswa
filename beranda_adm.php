023f7c1e1afbacf2f2dfe1526546a8896818e4c1
maudy88 <75382607+maudy88@users.noreply.github.com>
2021-01-28 11:33:55 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi</h3> 
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src=""></img></p>
+                          <p class="label-title" align="center"><strong>Absesi Mahasiswa</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman Absesni Kelas Open Source2<br>
+                            Melalui halaman ini dapat dilakukan absensi secara online  <br>
+                            dan juga pengelolaan laporan data rekapitulasi absensi.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data mahasiswa dan data rekap absensi.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
